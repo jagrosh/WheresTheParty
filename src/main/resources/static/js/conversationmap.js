8186eddb2f4520f5e088f116d5ccac7511b7fbaf
John Grosh <john.a.grosh@gmail.com>
2019-06-28 02:15:32 -0400

    fixed null guild icon

--- src/main/resources/static/js/conversationmap.js
+++ src/main/resources/static/js/conversationmap.js
@@ -20,7 +20,7 @@ function start() {
       if(!nodes.get(json.channel.id))
           nodes.add({id:json.channel.id, label:'#'+json.channel.name, group:json.guild.id, shape:'box', font: { color:'#2C2F33' }});
       if(!nodes.get(json.guild.id))
-          nodes.add({id:json.guild.id, label:json.guild.name, group:json.guild.id, image:json.guild.icon, shape:'image'});
+          nodes.add({id:json.guild.id, label:json.guild.name, group:json.guild.id, image:(json.guild.icon ? json.guild.icon : "brokenImage"), shape:'image'});
       if(!edges.get(json.user.id+"-"+json.channel.id))
           edges.add({id:json.user.id+"-"+json.channel.id, from:json.user.id, to:json.channel.id});
       if(!edges.get(json.channel.id+"-"+json.guild.id))
@@ -46,7 +46,7 @@ function start() {
         border: '#99AAB5',
         background: '#2C2F33'
       },
-      brokenImage: 'https://cdn0.iconfinder.com/data/icons/free-social-media-set/24/discord-512.png',
+      brokenImage: 'https://discordapp.com/assets/6debd47ed13483642cf09e832ed0bc1b.png',
       font: { color:'#99AAB5' }
     },
     edges: { color: '#99AAB5' }
