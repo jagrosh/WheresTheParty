05e5b58a6cb8fdb0ce6e455593a3d5453d082c44
John Grosh <john.a.grosh@gmail.com>
2019-06-28 19:04:41 -0400

    auto reconnect

--- src/main/resources/static/js/conversationmap.js
+++ src/main/resources/static/js/conversationmap.js
@@ -4,30 +4,37 @@ function start() {
   var brokenImage = 'https://discordapp.com/assets/6debd47ed13483642cf09e832ed0bc1b.png';
   
   // Connect to the websocket
-  var socket = new WebSocket(window.location.protocol.replace('http','ws') + '//' + window.location.hostname + '/ws');
-  socket.onopen = function(e) {
-    console.log('Successfully connected to server!');
+  connect = function() {
+    var socket = new WebSocket(window.location.protocol.replace('http','ws') + '//' + window.location.hostname + '/ws');
+    socket.onopen = function(e) {
+      console.log('Successfully connected to server!');
+    };
+    socket.onmessage = function(e) {
+      json = JSON.parse(e.data);
+      if(json.stats) {
+          if(json.stats.sessions)
+              document.getElementById('sessions').innerHTML = json.stats.sessions;
+          if(json.stats.servers)
+              document.getElementById('servers').innerHTML = json.stats.servers;
+      } else if(json.user) {
+        if(!nodes.get(json.user.id))
+            nodes.add({id:json.user.id, label:json.user.name, image:json.user.avatar, shape:'circularImage'});
+        if(!nodes.get(json.channel.id))
+            nodes.add({id:json.channel.id, label:'#'+json.channel.name, group:json.guild.id, shape:'box', font: { color:'#2C2F33' }});
+        if(!nodes.get(json.guild.id))
+            nodes.add({id:json.guild.id, label:json.guild.name, group:json.guild.id, image:(json.guild.icon ? json.guild.icon : brokenImage), shape:'image'});
+        if(!edges.get(json.user.id+"-"+json.channel.id))
+            edges.add({id:json.user.id+"-"+json.channel.id, from:json.user.id, to:json.channel.id});
+        if(!edges.get(json.channel.id+"-"+json.guild.id))
+            edges.add({id:json.channel.id+"-"+json.guild.id, from:json.channel.id, to:json.guild.id});
+      }
+    };
+    socket.onclose = function(e) {
+      console.log('Disconnected... Reconnecting in 30 seconds.');
+      setTimeout(connect, 30000);
+    };
   };
-  socket.onmessage = function(e) {
-    json = JSON.parse(e.data);
-    if(json.stats) {
-        if(json.stats.sessions)
-            document.getElementById('sessions').innerHTML = json.stats.sessions;
-        if(json.stats.servers)
-            document.getElementById('servers').innerHTML = json.stats.servers;
-    } else if(json.user) {
-      if(!nodes.get(json.user.id))
-          nodes.add({id:json.user.id, label:json.user.name, image:json.user.avatar, shape:'circularImage'});
-      if(!nodes.get(json.channel.id))
-          nodes.add({id:json.channel.id, label:'#'+json.channel.name, group:json.guild.id, shape:'box', font: { color:'#2C2F33' }});
-      if(!nodes.get(json.guild.id))
-          nodes.add({id:json.guild.id, label:json.guild.name, group:json.guild.id, image:(json.guild.icon ? json.guild.icon : brokenImage), shape:'image'});
-      if(!edges.get(json.user.id+"-"+json.channel.id))
-          edges.add({id:json.user.id+"-"+json.channel.id, from:json.user.id, to:json.channel.id});
-      if(!edges.get(json.channel.id+"-"+json.guild.id))
-          edges.add({id:json.channel.id+"-"+json.guild.id, from:json.channel.id, to:json.guild.id});
-    }
-  };
+  connect();
   
   // Create the veiwable network
   var container = document.getElementById('network');
