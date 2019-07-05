d8519aa05cea83d67eea71afca0dd027e79a3d0f
SnowyLuma <SnowyLuma@gmail.com>
2019-07-05 15:15:49 +0200

    Shorten WS URL building

--- src/main/resources/static/js/conversationmap.js
+++ src/main/resources/static/js/conversationmap.js
@@ -5,7 +5,7 @@ function start() {
   
   // Connect to the websocket
   connect = function() {
-    var socket = new WebSocket(window.location.protocol.replace('http','ws') + '//' + window.location.hostname + ':' + window.location.port + '/ws');
+    var socket = new WebSocket(window.location.href.replace('http', 'ws') + 'ws');
     socket.onopen = function(e) {
       console.log('Successfully connected to server!');
     };
