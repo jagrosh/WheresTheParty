525691d07a9b02057550b28219540d7c2266bef3
SnowyLuma <SnowyLuma@gmail.com>
2019-07-05 06:35:47 +0200

    Make nodes despawn

--- src/main/resources/static/js/conversationmap.js
+++ src/main/resources/static/js/conversationmap.js
@@ -5,7 +5,7 @@ function start() {
   
   // Connect to the websocket
   connect = function() {
-    var socket = new WebSocket(window.location.protocol.replace('http','ws') + '//' + window.location.hostname + '/ws');
+    var socket = new WebSocket(window.location.protocol.replace('http','ws') + '//' + window.location.hostname + ':' + window.location.port + '/ws');
     socket.onopen = function(e) {
       console.log('Successfully connected to server!');
     };
@@ -17,16 +17,38 @@ function start() {
           if(json.stats.servers)
               document.getElementById('servers').innerHTML = json.stats.servers;
       } else if(json.user) {
+        let data;
+        let now = new Date();
+
+        data = {id:json.user.id, label:json.user.name, image:json.user.avatar, shape:'circularImage', date:now};
         if(!nodes.get(json.user.id))
-            nodes.add({id:json.user.id, label:json.user.name, image:json.user.avatar, shape:'circularImage'});
+          nodes.add(data);
+        else
+          nodes.update(data);
+
+        data = {id:json.channel.id, label:'#'+json.channel.name, group:json.guild.id, shape:'box', font: { color:'#2C2F33' }, date:now};
         if(!nodes.get(json.channel.id))
-            nodes.add({id:json.channel.id, label:'#'+json.channel.name, group:json.guild.id, shape:'box', font: { color:'#2C2F33' }});
+          nodes.add(data);
+        else
+          nodes.update(data);
+
+        data = {id:json.guild.id, label:json.guild.name, group:json.guild.id, image:(json.guild.icon ? json.guild.icon : brokenImage), shape:'image', date:now};
         if(!nodes.get(json.guild.id))
-            nodes.add({id:json.guild.id, label:json.guild.name, group:json.guild.id, image:(json.guild.icon ? json.guild.icon : brokenImage), shape:'image'});
+          nodes.add(data);
+        else
+          nodes.update(data);
+
+        data = {id:json.user.id+"-"+json.channel.id, from:json.user.id, to:json.channel.id, date:now};
         if(!edges.get(json.user.id+"-"+json.channel.id))
-            edges.add({id:json.user.id+"-"+json.channel.id, from:json.user.id, to:json.channel.id});
+          edges.add(data);
+        else
+          edges.update(data);
+
+        data = {id:json.channel.id+"-"+json.guild.id, from:json.channel.id, to:json.guild.id, date:now};
         if(!edges.get(json.channel.id+"-"+json.guild.id))
-            edges.add({id:json.channel.id+"-"+json.guild.id, from:json.channel.id, to:json.guild.id});
+          edges.add(data);
+        else
+          edges.update(data);
       }
     };
     socket.onclose = function(e) {
@@ -35,7 +57,21 @@ function start() {
     };
   };
   connect();
-  
+
+  cleanup = function() {
+    let now = new Date()
+
+    filter = function (item) {
+      return now - item.date > 600000;
+    };
+
+    nodes.forEach(function (item) {nodes.remove(item.id)}, {filter: filter});
+    edges.forEach(function (item) {edges.remove(item.id)}, {filter: filter});
+
+    setTimeout(cleanup, 1000);
+  };
+  cleanup();
+
   // Create the veiwable network
   var container = document.getElementById('network');
   var data = {
@@ -71,4 +107,4 @@ function toggleInfobox() {
     im.style.display = "none";
     sb.style.display = "block";
   }
-} 
+}
